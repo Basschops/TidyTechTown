bd252305f53f08b66bafb26dd1b66a021911e644
Daragh O'Farrell <daraghofarrell@gmail.com>
2018-10-24 21:10:26 +0100

    Able to use user GPS to place marker. Needs to get GPS details on page load.

--- app/src/main/java/com/tt/t/tidytechtowns/MapsActivity.java
+++ app/src/main/java/com/tt/t/tidytechtowns/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.tt.t.tidytechtowns;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -8,6 +9,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -33,6 +35,8 @@ import java.util.Map;
         private Marker bin;
         private LocationManager locationManager;
         private LocationListener locationListener;
+        private LatLng currentLocation;
+        private Button gpsbutton;
 
         public void addBin(LatLng latlng) {
 
@@ -48,90 +52,100 @@ import java.util.Map;
                     .findFragmentById(R.id.map);
             mapFragment.getMapAsync(this);
 
-//        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-//        locationListener = new LocationListener() {
-//            @Override
-//            public void onLocationChanged(Location location) {
-//
-//            }
-//
-//            @Override
-//            public void onStatusChanged(String s, int i, Bundle bundle) {
-//
-//            }
-//
-//            @Override
-//            public void onProviderEnabled(String s) {
-//
-//            }
-//
-//            // If gps is turned off, open settings to turn it on
-//            @Override
-//            public void onProviderDisabled(String s) {
-//                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-//                startActivity(intent);
-//            }
-//        };
-//        //could check for version here. If <=23 will not need to ask for gps
-//        // if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            requestPermissions(new String[]{
-//                    Manifest.permission.ACCESS_FINE_LOCATION,
-//                    Manifest.permission.ACCESS_COARSE_LOCATION,
-//                    Manifest.permission.INTERNET
-//            }, 10);
-//            return;
-//        }
-//        //else {
-//        locationManager.requestLocationUpdates("gps", 10000, 20, locationListener);
-//    }
-//
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-//        switch (requestCode) {
-//            case 10:
-//                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-//                    locationManager.requestLocationUpdates("gps", 10000, 20, locationListener);
-//                return;
-//        }
-//    }
-
-            //locationManager.requestLocationUpdates("gps", 10000, 20, locationListener);
-
-
             Button binShow = (Button) findViewById(R.id.binBtn);
-            binShow.setOnClickListener(new View.OnClickListener() {
+            binShow.setOnClickListener(new View.OnClickListener(){
                 @Override
-                public void onClick(View view) {
+                public void onClick (View view){
                     if (!showing) {
                         LatLng bin1 = new LatLng(53, -6);
                         bin = mMap.addMarker(new MarkerOptions().position(bin1).title("This is a BIN!").draggable(true));
-                        Button button = (Button)findViewById(R.id.binBtn);
+                        Button button = (Button) findViewById(R.id.binBtn);
                         button.setText("Hide bins");
                         showing = true;
-                    }
-                    else{
+                    } else {
                         bin.remove();
-                        Button button = (Button)findViewById(R.id.binBtn);
+                        Button button = (Button) findViewById(R.id.binBtn);
                         button.setText("Show bins");
                         showing = false;
                     }
                 }
             });
 
-//        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-//            @Override
-//            public void onMapClick(LatLng latLng) {
-//
-//
-//                        "Lat : " + latLng.latitude + " , "
-//                                + "Long : " + latLng.longitude,
-//                        Toast.LENGTH_LONG).show();
-//
-//            }
-//        });
+            gpsbutton = (Button) findViewById(R.id.GPS);
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    currentLocation = new LatLng(location.getLongitude(), location.getLatitude());
+                    Toast toast = Toast.makeText(getApplicationContext(), "You there"+currentLocation.latitude, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
 
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                // If gps is turned off, open settings to turn it on
+                @Override
+                public void onProviderDisabled(String s) {
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            };
+
+            configureButton();
         }
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            switch (requestCode) {
+                case 10:
+                    configureButton();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        void configureButton() {
+            // first check for permissions
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
+                            , 10);
+                }
+                return;
+            }
+            // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+            gpsbutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //noinspection MissingPermission
+                    locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+                    String msg;
+                    if (currentLocation!=null){
+                        msg = currentLocation.longitude + " " + currentLocation.latitude;
+                        LatLng you = new LatLng(currentLocation.longitude, currentLocation.latitude);
+                        mMap.addMarker(new MarkerOptions().position(you).title("Yep, you!"));
+                    }
+                    else
+                        {msg = "Not available";}
+                    Toast toast = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            });
+        }
+
+
+
+
+
 
 
         /**
