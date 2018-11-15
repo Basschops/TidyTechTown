3272cc22f2efe3d72ae97597bd000d78112fa108
Mary Kate Tyrrell <mary.tyrrell@ucdconnect.ie>
2018-11-15 19:00:06 +0000

    adding input functionality and calculation to carbon calculator fragments.

--- app/src/main/java/com/tt/t/tidytechtowns/Tab1.java
+++ app/src/main/java/com/tt/t/tidytechtowns/Tab1.java
@@ -5,19 +5,48 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class Tab1 extends Fragment {
+import static java.lang.Float.parseFloat;
+
+public class Tab1 extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.tab1, container, false);
+
+        Button b = (Button) rootView.findViewById(R.id.button5);
+        b.setOnClickListener(this);
+
+        TextView plses = (TextView) rootView.findViewById(R.id.pls);
+        Resources resources = getResources();
+        String tt = resources.getString(R.string.total, 0.0);
+        plses.setText(tt);
+
         return rootView;
 
+    }
 
 
-        }
+    @Override
+    public void onClick(View v) {
 
+        TextView plses = (TextView) getView().findViewById(R.id.pls);
+        Resources resources = getResources();
+
+        EditText elec = (EditText) getView().findViewById(R.id.electricity);
+        int electricity = Integer.parseInt(elec.getText().toString());
+        Float x = parseFloat(String.valueOf(electricity));
+        String tt = resources.getString(R.string.total, x);
+        plses.setText(tt);
+
+    }
 
 }
+
+
