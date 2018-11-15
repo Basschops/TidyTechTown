5eca4c60922f65426baebe1a6c9b7151896b3ab1
SBean92 <sean.behan@ucdconnect.ie>
2018-11-15 00:52:34 +0000

    Added weather fucntions

--- app/src/main/java/com/tt/t/tidytechtowns/WeatherHTTPClient.java
+++ app/src/main/java/com/tt/t/tidytechtowns/WeatherHTTPClient.java
@@ -8,16 +8,18 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class WeatherHTTPClient {
-    private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=Dublin,ie&APPID={dd7d1ef2db1b92ecb2d1cadbf681df}";
+    private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
     private static String IMG_URL = "http://openweathermap.org/img/w/";
+    private static String API_KEY = "&APPID=dd7d1ef2db1b92ecb2d1cadbf681df72";
+    private static String FILE_EXT = ".png";
 
 
     public String getWeatherData(String location) {
         HttpURLConnection con = null ;
-        InputStream is = null;
+        InputStream stream = null;
 
         try {
-            con = (HttpURLConnection) ( new URL(BASE_URL + location)).openConnection();
+            con = (HttpURLConnection) ( new URL(BASE_URL + location + API_KEY)).openConnection();
             con.setRequestMethod("GET");
             con.setDoInput(true);
             con.setDoOutput(true);
@@ -25,13 +27,13 @@ public class WeatherHTTPClient {
 
             // Let's read the response
             StringBuffer buffer = new StringBuffer();
-            is = con.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            stream = con.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(stream));
             String line = null;
             while (  (line = br.readLine()) != null )
                 buffer.append(line + "\r\n");
 
-            is.close();
+            stream.close();
             con.disconnect();
             return buffer.toString();
         }
@@ -39,7 +41,7 @@ public class WeatherHTTPClient {
             t.printStackTrace();
         }
         finally {
-            try { is.close(); } catch(Throwable t) {}
+            try { stream.close(); } catch(Throwable t) {}
             try { con.disconnect(); } catch(Throwable t) {}
         }
 
@@ -48,31 +50,24 @@ public class WeatherHTTPClient {
     }
 
     public byte[] getImage(String code) {
-        HttpURLConnection con = null ;
-        InputStream is = null;
+        InputStream stream = null;
         try {
-            con = (HttpURLConnection) ( new URL(IMG_URL + code)).openConnection();
-            con.setRequestMethod("GET");
-            con.setDoInput(true);
-            con.setDoOutput(true);
-            con.connect();
-
-            // Let's read the response
-            is = con.getInputStream();
+            URL url = new URL(IMG_URL + code + FILE_EXT);
+            stream = (InputStream) url.getContent();
+            int bytesRead;
             byte[] buffer = new byte[1024];
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
 
-            while ( is.read(buffer) != -1)
-                baos.write(buffer);
-
-            return baos.toByteArray();
+            while ((bytesRead = stream.read(buffer)) != -1)
+                output.write(buffer, 0, bytesRead);
+            stream.close();
+            return output.toByteArray();
         }
         catch(Throwable t) {
             t.printStackTrace();
         }
         finally {
-            try { is.close(); } catch(Throwable t) {}
-            try { con.disconnect(); } catch(Throwable t) {}
+            try { stream.close(); } catch(Throwable t) {}
         }
 
         return null;
--- app/src/main/java/com/tt/t/tidytechtowns/Welcome.java
+++ app/src/main/java/com/tt/t/tidytechtowns/Welcome.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -14,35 +15,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.tt.t.tidytechtowns.weatherModels.Weather;
 
 import org.json.JSONException;
 
+import static java.lang.String.format;
+
 public class Welcome extends AppCompatActivity {
-
-
-    private Cursor bins;
-    private boolean lastBin = false;
-    private Cursor centers;
-    private Cursor communities;
-    private MyDatabase db;
-
 
     private DrawerLayout dl;
     private ActionBarDrawerToggle t;
     private NavigationView nv;
 
     private TextView cityText;
-    private TextView condDescr;
+    private TextView desc;
     private TextView temp;
-    private TextView press;
-    private TextView windSpeed;
-    private TextView windDeg;
+    private TextView tempMin;
+    private TextView tempMax;
+    private TextView direct;
+    private TextView speed;
+    private TextView progress;
+    private TextView appTemp;
 
-    private TextView hum;
-    private ImageView imgView;
+    private ProgressBar humid;
 
 
     @Override
@@ -52,12 +50,9 @@ public class Welcome extends AppCompatActivity {
 
 
 
-        db = new MyDatabase(this);
-        bins = db.getBins();
 
 
-
-        dl = (DrawerLayout) findViewById(R.id.activity_main);
+        dl = findViewById(R.id.activity_main);
         t = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
 
         dl.addDrawerListener(t);
@@ -68,7 +63,7 @@ public class Welcome extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-        nv = (NavigationView) findViewById(R.id.nv);
+        nv = findViewById(R.id.nv);
         nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -83,9 +78,6 @@ public class Welcome extends AppCompatActivity {
                         break;
                     case R.id.carbon: startCarbon(nv);
                         break;
-                    case R.id.carbonfootprint: startCarbonFootPrintCalculator(nv);
-                        break;
-
                     default:
                         return true;
                 }
@@ -95,19 +87,21 @@ public class Welcome extends AppCompatActivity {
             }
         });
 
-        String city = "London,UK";
+        String city = "Dublin,ie";
 
-        cityText = (TextView) findViewById(R.id.cityText);
-        condDescr = (TextView) findViewById(R.id.condDescr);
-        temp = (TextView) findViewById(R.id.temp);
-        hum = (TextView) findViewById(R.id.hum);
-        press = (TextView) findViewById(R.id.press);
-        windSpeed = (TextView) findViewById(R.id.windSpeed);
-        windDeg = (TextView) findViewById(R.id.windDeg);
-        imgView = (ImageView) findViewById(R.id.condIcon);
+        cityText = findViewById(R.id.cityText);
+        desc = findViewById(R.id.desc);
+        temp = findViewById(R.id.temp);
+        tempMin = findViewById(R.id.tempMin);
+        tempMax = findViewById(R.id.tempMax);
+        direct = findViewById(R.id.direct);
+        speed = findViewById(R.id.speed);
+        humid = findViewById(R.id.progressBar);
+        progress = findViewById(R.id.progress);
+        appTemp = findViewById(R.id.appTemp);
 
         JSONWeatherTask task = new JSONWeatherTask();
-        task.execute(new String[]{city});
+        task.execute(city);
 
 
 
@@ -150,24 +144,19 @@ public class Welcome extends AppCompatActivity {
         startActivity(i);
     }
 
-
-    public void startCarbonFootPrintCalculator(View v) {
-        Intent intent = new Intent(Welcome.this, CarbonFootprint.class);
-        startActivity(intent);
-    }
-
     private class JSONWeatherTask extends AsyncTask<String, Void, Weather> {
 
         @Override
         protected Weather doInBackground(String... params) {
             Weather weather = new Weather();
-            String data = (new WeatherHTTPClient().getWeatherData(params[0]));
+            String data = new WeatherHTTPClient().getWeatherData(params[0]);
 
             try {
                 weather = JSONWeatherParser.getWeather(data);
 
                 // Let's retrieve the icon
-                weather.iconData = (new WeatherHTTPClient().getImage(weather.currentCondition.getIcon()));
+                String icon = weather.currentCondition.getIcon();
+                weather.iconData = new WeatherHTTPClient().getImage(icon);
 
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -181,19 +170,58 @@ public class Welcome extends AppCompatActivity {
         protected void onPostExecute(Weather weather) {
             super.onPostExecute(weather);
 
-            if (weather.iconData != null && weather.iconData.length > 0) {
-                Bitmap img = BitmapFactory.decodeByteArray(weather.iconData, 0, weather.iconData.length);
-                imgView.setImageBitmap(img);
+//            if (weather.iconData != null && weather.iconData.length > 0) {
+//                Bitmap img = BitmapFactory.decodeByteArray(weather.iconData, 0, weather.iconData.length);
+//                imgView.setImageBitmap(img);
+//            }
+
+            cityText.setText(format("%s,%s", weather.location.getCity(), weather.location.getCountry()));
+            desc.setText(format("%s(%s)", weather.currentCondition.getCondition(), weather.currentCondition.getDescr()));
+            temp.setText(format("%d\u00b0", Math.round(weather.temperature.getTemp() - 273.15)));
+            tempMin.setText(format("%d\u00b0", Math.round(weather.temperature.getMinTemp() - 273.15)));
+            tempMax.setText(format("%d\u00b0", Math.round(weather.temperature.getMaxTemp() - 273.15)));
+            speed.setText(format("%1.2f m/s", weather.wind.getSpeed()));
+            float humidity = weather.currentCondition.getHumidity();
+            humid.setProgress(Integer.parseInt(format("%d", Math.round(humidity))));
+            progress.setText(format("%1.0f%%", humidity));
+            if (humidity >= 55f){
+                progress.setTextColor(Color.parseColor("#FFFFFF"));
+            }
+            float windAngle = weather.wind.getDeg();
+            if (windAngle > 337.5f || windAngle <= 22.5f){
+                direct.setText("North");
+            } else if (windAngle > 22.5f || windAngle <= 67.5f){
+                direct.setText("North East");
+            } else if (windAngle > 67.5f || windAngle <= 112.5f){
+                direct.setText("East");
+            } else if (windAngle > 112.5f || windAngle <= 157.5f){
+                direct.setText("South East");
+            } else if (windAngle > 157.5f || windAngle <= 202.5f){
+                direct.setText("South");
+            } else if (windAngle > 202.5f || windAngle <= 247.5){
+                direct.setText("South West");
+            } else if (windAngle > 247.5f || windAngle <= 292.5f){
+                direct.setText("West");
+            } else if (windAngle > 292.5f || windAngle <= 337.5f){
+                direct.setText("North West");
             }
 
-            cityText.setText(weather.location.getCity() + "," + weather.location.getCountry());
-            condDescr.setText(weather.currentCondition.getCondition() + "(" + weather.currentCondition.getDescr() + ")");
-            temp.setText("" + Math.round((weather.temperature.getTemp() - 273.15)) + "�C");
-            hum.setText("" + weather.currentCondition.getHumidity() + "%");
-            press.setText("" + weather.currentCondition.getPressure() + " hPa");
-            windSpeed.setText("" + weather.wind.getSpeed() + " mps");
-            windDeg.setText("" + weather.wind.getDeg() + "�");
-
+//            float tempC = weather.temperature.getTemp() - 273.15f;
+//            float wSpeed = weather.wind.getSpeed();
+//            float appTempC = tempC;
+//            if (tempC <= 10.5d){
+//                appTempC = (float) (13.12f + 0.6215f*tempC - 11.37f*(Math.pow(wSpeed, 0.16f))+0.3965f*tempC*(Math.pow(wSpeed, 0.16f)));
+//            } else if (tempC >= 26.66d){
+//                float tempF = tempC*1.8f + 32;
+//                float appTempF = (float) (42.38 + (2.05*tempF) + (10.14*humidity) - (0.22*tempF*humidity)
+//                        - (6.83*Math.pow(10, -3)*Math.pow(tempF, 2))
+//                        - (5.48*Math.pow(10, -2)*Math.pow(humidity, 2))
+//                        + (1.23*Math.pow(10d, -3)*Math.pow(tempF, 2)*humidity)
+//                        + (8.53*Math.pow(10d, -4)*tempF*Math.pow(humidity, 2))
+//                        - (1.99*Math.pow(10d, -6)*Math.pow(tempF, 2)*Math.pow(humidity, 2)));
+//                appTempC = 0.5556f*(appTempF-32f);
+//            }
+//            appTemp.setText(format("%1.2f\u00b0", appTempC));
         }
     }
 
